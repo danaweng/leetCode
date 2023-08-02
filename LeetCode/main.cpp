265e2180958d69ed9a07473f25ce96ed854bdb6c
danaweng <danaweng@tencent.com>
2023-08-02 11:12:37 +0800

    Initial Commit

---
+++ LeetCode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by danaweng on 2023/8/2.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
