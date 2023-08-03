b5c5de0f8e0087d7df7826c55f35ef59f202b68c
danaweng <danaweng@tencent.com>
2023-08-03 10:48:19 +0800

    fibonacci

--- LeetCode/DP/DP.cpp
+++ LeetCode/DP/DP.cpp
@@ -12,6 +12,46 @@
 #include <queue>
 using namespace std;
 
+//MARK: - 斐波那契数
+// https://leetcode.cn/problems/fibonacci-number/
+// 常规版本dp
+int fib1(int n) {
+    if (n < 2) {
+        return n;
+    }
+    // dp公式：dp[n] = dp[n-1] + dp[n-2]
+    int dp[n+1];
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n];
+}
+
+//空间优化版本dp
+int fib2(int n) {
+    //dp初始化
+    int dp[2];
+    dp[0] = 0;
+    dp[1] = 1;
+    // dp顺序
+    for (int i = 2; i <= n; i++) {
+        int sum = dp[0] + dp[1];
+        dp[0] = dp[1];
+        dp[1] = sum;
+    }
+    return dp[1];
+}
+
+// 递归版本
+int fib3(int n) {
+    if (n < 2) {
+        return n;
+    }
+    return fib3(n-1) + fib3(n - 2);
+}
+
 
 //MARK: - 动态规划
  int climbStairs(int n) {
